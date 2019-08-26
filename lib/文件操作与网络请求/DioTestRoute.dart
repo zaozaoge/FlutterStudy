8d1663f57b6d5d5db8af7a5ac2ca3da02cf226b7
jessie_hu <jessie_hu@enable-ets.com>
2019-08-26 17:51:21 +0800

    添加文件操作学习，原生HttpClient和第三方框架dio学习

---
+++ lib/文件操作与网络请求/DioTestRoute.dart
@@ -0,0 +1,52 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DioTestRoute());
+}
+
+class DioTestRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _DioTestRouteState();
+  }
+}
+
+class _DioTestRouteState extends State<DioTestRoute> {
+  Dio _dio = new Dio();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("DioTestRoute"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: FutureBuilder(
+              future:
+                  _dio.get("https://api.github.com/orgs/flutterchina/repos"),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                //请求完成
+                if (snapshot.connectionState == ConnectionState.done) {
+                  Response response = snapshot.data;
+                  //发生错误
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  }
+                  //请求成功，通过项目信息构建用于显示项目名称的ListView
+                  print(response.data.toString());
+                  return ListView(
+                    children: response.data.map<Widget>((e) =>
+                        ListTile(title: Text(e["full_name"]))
+                    ).toList(),
+                  );
+                }
+                return CircularProgressIndicator();
+              }),
+        ),
+      ),
+    );
+  }
+}
--- lib/文件操作与网络请求/FileOperationRoute.dart
+++ lib/文件操作与网络请求/FileOperationRoute.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
+void main() {
+  runApp(FileOperationRoute());
+}
+
 class FileOperationRoute extends StatefulWidget {
   FileOperationRoute({Key key}) : super(key: key);
 
@@ -18,6 +22,12 @@ class _FileOperationRouteState extends State<FileOperationRoute> {
   @override
   void initState() {
     super.initState();
+    //从文件读取点击次数
+    _readCounter().then((int value) {
+      setState(() {
+        _counter = value;
+      });
+    });
   }
 
   Future<File> _getLocalFile() async {
@@ -27,13 +37,40 @@ class _FileOperationRouteState extends State<FileOperationRoute> {
   }
 
   Future<int> _readCounter() async {
-    try {} on FileSystemException {
+    try {
+      File file = await _getLocalFile();
+      //读取点击次数（以字符串）
+      String contents = await file.readAsString();
+      return int.parse(contents);
+    } on FileSystemException {
       return 0;
     }
   }
 
+  Future<Null> _incrementCounter() async {
+    setState(() {
+      _counter++;
+    });
+    //将点击次数以字符串类型写入文件中
+    await (await _getLocalFile()).writeAsString('$_counter');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("文件操作"),
+        ),
+        body: Center(
+          child: Text('点击了$_counter 次'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: "Increment",
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
